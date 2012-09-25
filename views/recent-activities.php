e3c2d04f4a1401ec0f040f34f3b193b9b106ef92
Dan Crimmins <dbcrimmins@gmail.com>
2012-09-25 11:12:06 -0500

    Added truncation to excerpt in recent activities widget

--- views/recent-activities.php
+++ views/recent-activities.php
@@ -1,28 +1,33 @@
-<section class="span12">
-    <section class="content-container recent-activity">
+<article class="span12 content-container recent_activity">
 
-        <hgroup class="content-header"><h3><?php echo $recent_act_title;?></h3></hgroup>
+	<hgroup class="content-header">
+  	<h3><?php echo $recent_act_title;?></h3>
+  </hgroup>
 
-        <ol class="content-body result clearfix">
+	<section class="content-body clearfix">
+		<ul class="recent-activity-list">
 
 <?php 
 
+    $activities_text = array(
+        'question' => 'Asked this: ',
+        'answer'   => 'Answered this: ',
+        ''         => 'Commented this: ',
+        'comment'  => 'Commented this: ',
+        'post'     => 'Posted this: ',
+        'guide'    => 'Posted this: '
+    );
 
-$activities_text = array('question' 	=> 'Asked this: ',
-							'answer'	=> 'Answered this: ',
-							''			=> 'Commented this: ',
-							'comment'	=> 'Commented this: ',
-							'post'		=> 'Posted this: ',
-							'guide'		=> 'Posted this: ');
-							
-$action_text = array('follow' => 'Followed this: ',
-					 'upvote' => 'Liked this: ');
+    $action_text = array(
+        'follow' => 'Followed this: ',
+        'upvote' => 'Liked this: '
+    );
 
     foreach($activities as $activity):
 
         $activity_text = (array_key_exists($activity->action, $action_text)) ? $action_text[$activity->action] : $activities_text[$activity->type];
 
-        $excerpt = '<article class="excerpt">' . ( strlen( $activity->content ) > 200 ? substr( $activity->content, 0, 200 ) . "&#8230;" : $activity->content ) . '</article>';
+        $excerpt = '<li class="recent-activity_excerpt">' . ( strlen( $activity->content ) > 200 ? substr( truncated_text(sanitize_text($activity->content)), 0, 200 ) . "&#8230;" : truncated_text(sanitize_text($activity->content))) . '</li>';
 
         $time_options = array(
             "timestamp"   => strtotime( $activity->date ),
@@ -32,28 +37,28 @@ $action_text = array('follow' => 'Followed this: ',
         );
 
 ?>
-
-
-            <li class="clearfix">
-                <?php get_partial( 'parts/crest', array( "user_id" => $activity->author, "width" => 'span4' ) ); ?>
-                <div class="span8">
-                    <h3>
-                        <span><?php echo $activity_text; ?></span>
-                        <?php get_partial( 'parts/space_date_time', $time_options ); ?>
-                        <a href="<?php echo (in_array($activity->type, $recent->comment_types)) ? ((count($activity->post->category)) ? get_term_link($activity->post->category[0]) : null) : ((count($activity->category)) ? get_term_link($activity->category[0]) : null) ;?>" class="category"><?php echo (in_array($activity->type, $recent->comment_types)) ? ((count($activity->post->category)) ? $activity->post->category[0]->cat_name : 'Uncategorized') : ((count($activity->category)) ? $activity->category[0]->cat_name : 'Uncategorized') ;?></a>
-
-                        <a href="<?php echo (in_array($activity->type, $recent->comment_types)) ? get_permalink($activity->post->ID) : get_permalink($activity->ID) ;?>"><?php echo (in_array($activity->type, $recent->comment_types)) ? sanitize_text($activity->post->post_title) : sanitize_text($activity->title);?></a>
-                    </h3>
-                    <?php echo (in_array($activity->type, $recent->comment_types)) ? sanitize_text($excerpt) : null; ?>
-
-                </div>
-            </li>
+					<li class="clearfix">
+						<div class="recent-activity_avatar">
+							<?php echo profile_photo( $activity->author ); ?>
+						</div>
+						<div class="recent-activity_data">
+							<ul>
+								<li class="recent-activity_activity"><?php echo return_screenname_link( $activity->author ); ?> <?php echo $activity_text; ?></li>
+								<li class="recent-activity_category-time">
+									<?php get_partial( 'parts/space_date_time', $time_options ); ?>
+									<a href="<?php echo (in_array($activity->type, $recent->comment_types)) ? ((count($activity->post->category)) ? get_term_link($activity->post->category[0]) : null) : ((count($activity->category)) ? get_term_link($activity->category[0]) : null) ;?>" class="category"><?php echo (in_array($activity->type, $recent->comment_types)) ? ((count($activity->post->category)) ? $activity->post->category[0]->cat_name : 'Uncategorized') : ((count($activity->category)) ? $activity->category[0]->cat_name : 'Uncategorized') ;?></a>
+								</li>
+								<li class="recent-activity_title"><a href="<?php echo (in_array($activity->type, $recent->comment_types)) ? get_permalink($activity->post->ID) : get_permalink($activity->ID) ;?>"><?php echo (in_array($activity->type, $recent->comment_types)) ? truncated_text(sanitize_text($activity->post->post_title)) : truncated_text(sanitize_text($activity->title));?></a></li>
+								<?php if (in_array($activity->type, $recent->comment_types) || $activity->type == '') {echo $excerpt;} ?>
+							</ul>
+						</div>
+					</li>
 
 <?php
     endforeach; 
 ?>
+		</ul>
+		
+	</section>			
 
-        </ol>
-
-    </section>
-</section>
+</article>
